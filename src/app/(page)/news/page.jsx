248bf26ea6b992b865023173c13daa01de426c03
ABUQORIgggg <abuxzzzzzzz@gmail.com>
2024-09-28 16:49:18 +0500

    products

--- src/app/(page)/news/page.jsx
+++ src/app/(page)/news/page.jsx
@@ -21,7 +21,9 @@ export default function News() {
 
     getNews();
   }, []);
+  
 
+  
   return (
     <div className="max-w-3xl mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6">Новости:</h1>
