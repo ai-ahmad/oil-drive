bafca22961ae58489416efaaaf42d2fa906585dc
ABUQORIgggg <abuxzzzzzzz@gmail.com>
2024-09-28 16:51:12 +0500

    test

--- src/app/(page)/news/page.jsx
+++ src/app/(page)/news/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import axios from 'axios';
 import Image from 'next/image';
@@ -16,14 +16,12 @@ export default function News() {
         setNews(response.data);
       } catch (error) {
         console.error('Error fetching news:', error);
-      
+      }
     };
-
+    
     getNews();
   }, []);
-  
 
-  
   return (
     <div className="max-w-3xl mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6">Новости:</h1>
