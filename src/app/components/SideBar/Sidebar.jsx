7bf94bf769169a3fcbc50211c50ca70f0d50aaea
dior011 <diyorraxmatullaev3@gmail.com>
2024-10-02 19:36:02 +0500

    ss

--- src/app/components/SideBar/Sidebar.jsx
+++ src/app/components/SideBar/Sidebar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Cattegory from './Cattegory'
-import PopularProducts from './PopularProducts'
+import Cattegory from './Category'
+import PopularProducts from './PopularProduct'
 
 const Sidebar = () => {
     return (
-        <div className='flex flex-col gap-[30px]'>
+        <div className='flex flex-col gap-7 '>
             <Cattegory />
             <PopularProducts />
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,34 +18,9 @@ module.exports = {
         'adaptive-xl': 'clamp(1.25rem, 3.5vw, 2.25rem)',
         'adaptive-xxl': 'clamp(1.5rem, 4vw, 2.5rem)',
       },
-      padding: {
-        'adaptive-sm': 'clamp(8px, 2vw, 16px)',   
-        'adaptive-md': 'clamp(12px, 2.5vw, 24px)', 
-        'adaptive-lg': 'clamp(16px, 3vw, 32px)',   
-        'adaptive-xl': 'clamp(20px, 3.5vw, 40px)', 
-        'adaptive-xxl': 'clamp(24px, 4vw, 48px)',  
-      },
-      margin: {
-        'adaptive-sm': 'clamp(8px, 2vw, 16px)',   
-        'adaptive-md': 'clamp(12px, 2.5vw, 24px)', 
-        'adaptive-lg': 'clamp(16px, 3vw, 32px)',   
-        'adaptive-xl': 'clamp(20px, 3.5vw, 40px)', 
-        'adaptive-xxl': 'clamp(24px, 4vw, 48px)',  
-      },
-      width: {
-        'adaptive-sm': 'clamp(200px, 50vw, 400px)',   
-        'adaptive-md': 'clamp(300px, 55vw, 500px)',   
-        'adaptive-lg': 'clamp(400px, 60vw, 600px)',   
-        'adaptive-xl': 'clamp(500px, 65vw, 700px)',   
-        'adaptive-xxl': 'clamp(600px, 70vw, 800px)',  
-      },
-      height: {  // Исправлено на правильное "height"
-        'adaptive-sm': 'clamp(200px, 50vw, 400px)',   
-        'adaptive-md': 'clamp(300px, 55vw, 500px)',   
-        'adaptive-lg': 'clamp(400px, 60vw, 600px)',   
-        'adaptive-xl': 'clamp(500px, 65vw, 700px)',   
-        'adaptive-xxl': 'clamp(600px, 70vw, 800px)',  
-      },
+    
+    
+   
     },
   },
   plugins: [],
