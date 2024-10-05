7cf2f0488fe0547c316cb5c74ec37f0c2ced0b9b
dior011 <diyorraxmatullaev3@gmail.com>
2024-10-05 22:59:27 +0500

    test

--- src/app/(page)/news/page.jsx
+++ src/app/(page)/news/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Loading from '@/app/components/Loading/Loading';
 import Navigation from '@/app/components/Navigations/Header';
 import axios from 'axios';
 import Image from 'next/image';
@@ -8,6 +9,7 @@ import { useEffect, useState } from 'react';
 
 export default function News() {
   const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getNews = async () => {
@@ -17,40 +19,47 @@ export default function News() {
         setNews(response.data);
       } catch (error) {
         console.error('Error fetching news:', error);
+      } finally{
+        setLoading(false)
       }
     };
 
     getNews();
   }, []);
-
+  if(loading){
+    return <Loading/>
+  }
   return (
    <>
    <Navigation/>
-     <div className="max-w-3xl mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-6">Новости:</h1>
+    <div className="flex justify-center">
+    <div className="max-w-3xl mx-auto p-6 ">
+    <h1 className="text-3xl font-bold mb-6">Новости:</h1>
 
-      <div className="space-y-8">
-        {news.map((item) => (
-          <article key={item._id} className="border-b pb-8">
-            <Image
-              src={item.images && item.images.length > 0 ? `http://localhost:5000${item.images[0]}` : '/default.jpg'}
-              alt={item.title}
-              width={400}
-              height={200}
-              className="w-full h-auto mb-4"
-              priority={true}
-            />
+    <div className="space-y-8">
+    {news.map((item) => (
+      <article key={item._id} className="border-b pb-8">
+      <Image
+      src={item.images && item.images.length > 0 ? `http://localhost:5000${item.images[0]}` : '/default.jpg'}
+      alt={item.title}
+      width={400}
+      height={200}
+      className="w-full h-auto mb-4"
+      priority={true}
+      />
             <time className="text-sm text-gray-500">{item.date}</time>
             <h2 className="text-xl font-semibold mt-2 mb-2">
-              <a href={`/news/${item._id}`} className="text-red-600 hover:underline">
+            <a href={`/news/${item._id}`} className="text-red-600 hover:underline">
                 {item.title}
               </a>
-            </h2>
-            <p className="text-gray-700">{item.description1}</p>
-          </article>
-        ))}
-      </div>
-    </div>
-   </>
-  );
-}
+              </h2>
+              <p className="text-gray-700">{item.description1}</p>
+              </article>
+              ))}
+              </div>
+              </div>
+              </div>
+              </>
+              );
+              }
+              
