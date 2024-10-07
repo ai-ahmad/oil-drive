ee92e88db1a06ecd7bd94c8354881b4761329ca1
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-10-07 20:58:48 +0500

    ASD

--- src/app/components/Banner/Baner.jsx
+++ src/app/components/Banner/Baner.jsx
@@ -7,8 +7,9 @@ const Baner = () => {
   const [currentSlide, setCurrentSlide] = useState(0); 
   const [isDescriptionVisible, setIsDescriptionVisible] = useState(false); 
   const [isImageVisible, setIsImageVisible] = useState(true); 
-  const apiUrl = process.env.NEXT_PUBLIC_OILDRIVE_API
-  const imgUrl = process.env.NEXT_PUBLIC_OILDRIVE_IMG_API
+  const apiUrl = process.env.NEXT_PUBLIC_OILDRIVE_API;
+  const imgUrl = process.env.NEXT_PUBLIC_OILDRIVE_IMG_API;
+
   const fetchSlides = async () => {
     try {
       const response = await fetch(`${apiUrl}/banner`);
@@ -60,7 +61,7 @@ const Baner = () => {
   };
 
   return (
-    <div className="p-7 relative w-[1000px] h-[350px] mx-auto overflow-hidden">
+    <div className="p-7 relative w-full h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] max-w-7xl mx-auto overflow-hidden">
       {slides.length > 0 && (
         <img
           src={`${imgUrl}${slides[currentSlide].image_url}`} 
@@ -75,7 +76,7 @@ const Baner = () => {
         <div
           className={`absolute bottom-10 transform transition-transform duration-700 ${
             isDescriptionVisible ? 'opacity-100' : 'opacity-0'
-          } bg-red-500 text-white p-6 rounded text-lg w-[300px] h-[70px]`}
+          } bg-red-500 text-white p-4 md:p-6 rounded text-xs sm:text-sm md:text-lg w-[250px] sm:w-[300px] lg:w-[400px] h-[50px] sm:h-[70px] lg:h-[90px]`}
           style={{
             right: '0%', 
             transform: isDescriptionVisible ? 'translateX(-600px)' : 'translateX(100%)', 
@@ -90,15 +91,15 @@ const Baner = () => {
 
       <button 
         onClick={handlePrevSlide} 
-        className="absolute left-5 top-1/2 transform -translate-y-1/2 p-0 rounded-full transition duration-300"
+        className="absolute left-3 sm:left-5 top-1/2 transform -translate-y-1/2 p-0 rounded-full transition duration-300"
       >
-        <span className=" m-5 text-4xl text-black hover:text-white">&#10094;</span> 
+        <span className="m-3 sm:m-5 text-2xl sm:text-4xl text-black hover:text-white">&#10094;</span> 
       </button>
       <button 
         onClick={handleNextSlide} 
-        className="absolute right-5 top-1/2 transform -translate-y-1/2 p-0 rounded-full transition duration-300"
+        className="absolute right-3 sm:right-5 top-1/2 transform -translate-y-1/2 p-0 rounded-full transition duration-300"
       >
-        <span className=" m-5 text-4xl text-black hover:text-white">&#10095;</span> 
+        <span className="m-3 sm:m-5 text-2xl sm:text-4xl text-black hover:text-white">&#10095;</span> 
       </button>
     </div>
   );
