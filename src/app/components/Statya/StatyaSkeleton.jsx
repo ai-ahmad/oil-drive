a672a18e994753104f02a779e657341b0a8d225d
sardor <madalievsardor33@gmail.com>
2025-01-11 21:42:55 +0500

    ASD

--- src/app/components/Statya/StatyaSkeleton.jsx
+++ src/app/components/Statya/StatyaSkeleton.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const StatyaSkeleton = () => { 
   return ( 
     <div className="border border-gray-300 bg-white shadow-lg transition-transform duration-200 h-80 w-full flex flex-col  mb-4 rounded-xl"> 
-      <div className="w-full h-80 bg-gray-200 skeleton rounded-t-xl rounded-b-none mb-4"></div> 
-      <div className="h-16 w-full bg-gray-200 skeleton rounded-b-xl rounded-t-none mt-auto"></div> 
+      <div className="w-full h-64 sm:h-72 md:h-80 lg:h-96 bg-gray-200 skeleton rounded-t-xl rounded-b-none mb-4"></div> 
+      <div className="h-16 sm:h-12 md:h-18 lg:h-20 w-full bg-gray-200 skeleton rounded-b-xl rounded-t-none mt-auto"></div> 
     </div> 
   ); 
 }; 
