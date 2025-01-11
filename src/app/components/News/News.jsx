87474cbc4de7a332b86eb56f1bed868b5efb20fe
d1lmurodvic <dildoraishmuhamedova4@gmail.com>
2025-01-11 22:11:53 +0500

    asdf

--- src/app/components/News/News.jsx
+++ src/app/components/News/News.jsx
@@ -54,8 +54,8 @@ const News = () => {
     return (
       <div className="container mx-auto py-4 w-full ">
         <div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {Array.from({ length: 1 }).map((_, index) => (
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+            {Array.from({ length: 4 }).map((_, index) => (
               <NewsSkeleton key={index} />
             ))}    
           </div>
--- src/app/components/News/NewsSkeleton.jsx
+++ src/app/components/News/NewsSkeleton.jsx
@@ -1,18 +1,11 @@
-  import React from "react";
+import React from "react";
 
-  const NewsSkeleton = () => {
-    return (
-      <div className="flex  gap-6 bg-white p-4 relative left-8">
-        <div className="flex-1 min-w-[200%]">
-          <div className="skeleton h-44 w-full md:min-w-[45%] bg-gray-100 mb-4"></div>
-          <div className="skeleton h-44 w-full bg-gray-100"></div>
-        </div>
-        <div className="flex-1 min-w-[200%]">
-          <div className="skeleton h-44 w-full bg-gray-100 mb-4"></div>
-          <div className="skeleton h-44 w-full bg-gray-100"></div>
-        </div>
-      </div>
-    );
-  };
+const NewsSkeleton = () => {
+  return (
+    <div className="flex gap-6 bg-white ">
+      <div className="skeleton h-44 w-full bg-gray-100 mb-4"></div>
+    </div>
+  );
+};
 
-  export default NewsSkeleton;
+export default NewsSkeleton;
