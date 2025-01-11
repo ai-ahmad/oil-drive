a8242705a6a90eb113e7a0ef7725cf5861d927a8
d1lmurodvic <dildoraishmuhamedova4@gmail.com>
2025-01-11 22:19:37 +0500

    asd

--- src/app/components/News/NewsSkeleton.jsx
+++ src/app/components/News/NewsSkeleton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NewsSkeleton = () => {
   return (
     <div className="flex gap-6 bg-white ">
-      <div className="skeleton h-44 w-full bg-gray-100 mb-4"></div>
+      <div className="skeleton h-44 md:h-44 lg:h-56 w-full bg-gray-100 mb-4"></div>
     </div>
   );
 };
