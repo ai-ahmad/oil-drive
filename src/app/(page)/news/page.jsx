7014c880f41cfa11638a91ebc8d05f478b67003a
d1lmurodvic <dildoraishmuhamedova4@gmail.com>
2025-01-11 15:32:07 +0500

    asd

--- src/app/(page)/news/page.jsx
+++ src/app/(page)/news/page.jsx
@@ -4,6 +4,7 @@ import Loading from "@/app/components/Loading/Loading";
 import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
+import PageSkeleton from "./PageSkeleton";
 import { useEffect, useState } from "react";
 
 export default function News() {
@@ -30,7 +31,18 @@ export default function News() {
   }, [apiUrl]);
 
   if (loading) {
-    return <Loading />;
+    return (
+      <div className="container mx-auto py-4 w-full">
+        <div className="border border-gray-300 bg-white shadow-lg w-full max-w-screen-xl p-6 rounded-lg">
+          <h2 className="text-black text-2xl mb-4">Новости:</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+            {Array.from({ length: 4 }).map((_, index) => (
+              <PageSkeleton key={index} />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -59,7 +71,7 @@ export default function News() {
               />
               <time className="text-sm text-gray-500 block mb-2">{item.date}</time>
               <h2 className="text-lg font-semibold text-gray-800 mb-2">
-                <span className="text-red-600 hover:underline">{item.title}</span>
+                <span className="text-red-600 s hover:underline">{item.title}</span>
               </h2>
               <p className="text-gray-700 text-sm sm:text-base">{item.description1}</p>
             </article>
--- src/app/components/News/News.jsx
+++ src/app/components/News/News.jsx
@@ -14,28 +14,32 @@ const News = () => {
       title: "Моторные масла",
       description: "Описание моторных масел и их характеристик.",
       date: "01/01/2024",
-      image: "https://ru.lukoil-shop.com/_next/image/?url=%2Fimages%2Fpng%2FmainCatalog%2Foil1v2.png&w=384&q=75",
+      image:
+        "https://ru.lukoil-shop.com/_next/image/?url=%2Fimages%2Fpng%2FmainCatalog%2Foil1v2.png&w=384&q=75",
     },
     {
       id: "2",
       title: "Трансмиссионные масла",
       description: "Информация о трансмиссионных маслах.",
       date: "02/01/2024",
-      image: "https://ru.lukoil-shop.com/_next/image/?url=%2Fimages%2Fpng%2FmainCatalog%2Foil2v2.png&w=384&q=75",
+      image:
+        "https://ru.lukoil-shop.com/_next/image/?url=%2Fimages%2Fpng%2FmainCatalog%2Foil2v2.png&w=384&q=75",
     },
     {
       id: "3",
       title: "Охлаждающие жидкости",
       description: "Описание охлаждающих жидкостей и их преимущества.",
       date: "03/01/2024",
-      image: "https://ru.lukoil-shop.com/_next/image/?url=%2Fimages%2Fpng%2FmainCatalog%2Foil3v2.png&w=384&q=75",
+      image:
+        "https://ru.lukoil-shop.com/_next/image/?url=%2Fimages%2Fpng%2FmainCatalog%2Foil3v2.png&w=384&q=75",
     },
     {
       id: "4",
       title: "Тормозные жидкости",
       description: "Особенности тормозных жидкостей.",
       date: "04/01/2024",
-      image: "https://ru.lukoil-shop.com/_next/image/?url=%2Fimages%2Fpng%2FmainCatalog%2Foil4v2.png&w=384&q=75",
+      image:
+        "https://ru.lukoil-shop.com/_next/image/?url=%2Fimages%2Fpng%2FmainCatalog%2Foil4v2.png&w=384&q=75",
     },
   ];
 
@@ -79,11 +83,13 @@ const News = () => {
               className="bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300 flex"
             >
               <div className="flex-1 p-6 text-left flex flex-col justify-center">
-                <h1 className="text-xl md:text-3xl font-bold text-gray-800 mb-4">{news.title}</h1>
+                <h1 className="text-xl md:text-3xl font-bold text-gray-800 mb-4">
+                  {news.title}
+                </h1>
                 <div className="text-gray-400 text-sm flex items-center gap-4 mt-auto">
                   <span className="text-lg font-medium"></span>
                   <div className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-100 hover:bg-gray-200 transition-colors duration-300">
-                    <FaArrowRight  className="text-gray-800 text-lg md:text-2xl" />
+                    <FaArrowRight className="text-gray-800 text-lg md:text-2xl" />
                   </div>
                 </div>
               </div>
